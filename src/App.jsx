9cf736786bad547765b75a7511fe228738c26b74
callmerem <nodlehaki@gmail.com>
2026-01-07 08:22:31 +0800

    save

--- src/App.jsx
+++ src/App.jsx
@@ -367,6 +367,7 @@ const ALL_LLMS = [
   { id: 'Nautilus', name: 'Nautilus', url: 'https://github.com/nautechsystems/nautilus_trader', icon: 'https://avatars.githubusercontent.com/u/33360632?s=48&v=4', category: 'Trading' },
   { id: 'Lacuna', name: 'Lacuna', url: 'https://humanplane.com/lacuna', icon: 'https://avatars.githubusercontent.com/u/240769383?s=48&v=4', category: 'Trading' },
   { id: 'HKUDS AI-Trader', name: 'HKUDS AI-Trader', url: 'https://ai4trade.ai/index.html', icon: 'https://avatars.githubusercontent.com/u/118165258?s=48&v=4', category: 'Trading' },
+  { id: 'FinceptTerminal', name: 'FinceptTerminal', url: 'https://github.com/Fincept-Corporation/FinceptTerminal?tab=License-1-ov-file', icon: 'https://avatars.githubusercontent.com/u/178755995?s=48&v=4', category: 'Trading' },
 
 
 ];
